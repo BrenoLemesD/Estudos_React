9eee477762814bb5aced7f7c72278a83c1b9d53a
BrenoLemesD <brenolemesd@gmail.com>
2024-10-08 14:27:47 -0300

    Atualizações da aula 52

--- 3_Avançando_em_React/avancando/src/App.jsx
+++ 3_Avançando_em_React/avancando/src/App.jsx
@@ -9,6 +9,8 @@ import CarDetails from './components/CarDetails'
 import Fragment from './components/Fragment'
 import Container from './components/Container'
 import ExecuteFunction from './components/ExecuteFunction'
+import Message from './components/Message'
+import ChangeMessageState from './components/ChangeMessageState'
 
 
 function App() {
@@ -24,6 +26,13 @@ function App() {
   function showMessage() {
     console.log("Evento do componente pai!")
   }
+
+  const [message, setMessage] = useState("")
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  }
+
   return (
     <>
       <div className='App'>
@@ -61,6 +70,9 @@ function App() {
         </Container>
         {/* Executar Função com props */}
         <ExecuteFunction myFunction={showMessage}/>
+        {/* State Lift */}
+        <Message msg={message}/>
+        <ChangeMessageState handleMessage={handleMessage}/>
       </div>
     </>
   )
---
+++ 4_CSS_React/src/components/MyComponents.js
@@ -0,0 +1,7 @@
+import './MyComponents.css';
+
+const MyComponents = () => {
+    return <div>myComponents</div>
+}
+
+export default MyComponents
