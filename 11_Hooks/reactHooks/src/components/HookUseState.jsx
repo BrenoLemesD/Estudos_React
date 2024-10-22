7b7b89849eab2559fc9f6ab80dbed1faab4883f4
BrenoLemesD <brenolemesd@gmail.com>
2024-10-22 13:11:57 -0300

    Iniciei o proximo capitulo

--- 11_Hooks/reactHooks/src/components/HookUseState.jsx
+++ 11_Hooks/reactHooks/src/components/HookUseState.jsx
@@ -31,7 +31,7 @@ const HookUseState = () => {
       <p>Variável: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Change!</button>
-      {/* 2 - useState com inputs */}
+      {/* 2 - useState e inputs */}
       <p>Digite sua idade:</p>
       <form onSubmit={handleSubmit}>
         <input
